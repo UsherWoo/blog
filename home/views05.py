9bd6eff1b2fb45d073240779c2835d901291551c
usher <usher@localhost.localdomain>
2025-06-30 01:05:25 +0800

    add requirement

--- home/views05.py
+++ home/views05.py
@@ -2,6 +2,10 @@
 home/views05.py
 '''
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as sys_login
+from django.contrib.auth import logout as sys_logout
+from django.contrib.auth.models import User
 from datetime import datetime
 Member = {
     '王小明':'123',
@@ -11,22 +15,32 @@ Member = {
 def login(request):    #定義網頁函式
     now = datetime.now()
     message = ''
+    user = None
     try:
         name = request.GET['name']
         password = request.GET['password']
     except:
         name = None       #尚未填表單
     if name != None:
-        if name in Member:
-            if password == Member[name]:
+#       if name in Member:
+#           if password == Member[name]:
+#       user = authenticate(username=name,password=password)    
+#       if user:
+        try:
+            User.objects.get(username=name)
+            user = authenticate(username=name,password=password)
+            if user is not None:
                 message = name + ':您好'
-                request.session['userName'] = name
+    #           request.session['userName'] = name
+                sys_login(request,user)
             else:
                 message = '密碼錯誤'
-        else:
+        except:
+#       else
             message = name + ':尚未註冊'
     return render(request,'index05.html',locals())
 
 def logout(request):
-    request.session['userName'] = None
+#    request.session['userName'] = None
+    sys_logout(request)
     return redirect('/') 
